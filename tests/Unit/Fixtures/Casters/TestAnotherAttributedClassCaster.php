9c6d9e428ef639d1c9231202e36a77a5e23c5e5d
Dezer <i@appdevelop.ru>
2022-11-29 01:31:05 +0100

    Add default caster

--- tests/Unit/Fixtures/Casters/TestAnotherAttributedClassCaster.php
+++ tests/Unit/Fixtures/Casters/TestAnotherAttributedClassCaster.php
@@ -5,23 +5,23 @@ declare(strict_types=1);
 namespace Dezer32\Libraries\Dto\Test\Unit\Fixtures\Casters;
 
 use Dezer32\Libraries\Dto\Contracts\CasterInterface;
-use Dezer32\Libraries\Dto\Test\Unit\Fixtures\AnotherClasses\AnotherClass;
+use Dezer32\Libraries\Dto\Test\Unit\Fixtures\AnotherClasses\AnotherAttributedClass;
 
-class TestAnotherClassCaster implements CasterInterface
+class TestAnotherAttributedClassCaster implements CasterInterface
 {
     public function __construct(
         private string $salt,
     ) {
     }
 
-    public function cast(mixed $value): AnotherClass
+    public function cast(mixed $value): AnotherAttributedClass
     {
-        if (is_a($value, AnotherClass::class)) {
+        if (is_a($value, AnotherAttributedClass::class)) {
             return $value;
         }
 
         $text = sprintf('%s.%s.%s', $this->salt, $value['text'], $this->salt);
 
-        return new AnotherClass($text);
+        return new AnotherAttributedClass($text);
     }
 }
